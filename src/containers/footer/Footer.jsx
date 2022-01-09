f35684b027d9da9cb2e43fc16e896cbdb3fcafab
Nahyan Sharvin <nskonnalath@gmail.com>
2022-01-09 22:51:29 +0530

    feat: footer address added

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import { SocialMedia } from '../../components'
 import './footer.css'
 
+//Material Icons
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmailIcon from '@mui/icons-material/Email';
+import CallIcon from '@mui/icons-material/Call';
+
 function Footer() {
     return (
         <div className='footer section-padding'>
+            <div className='contact-details'>
+                <div className='contact-details-wrapper'>
+                    <div className='address contact-details-child'>
+                        <LocationOnIcon fontSize='large' sx={{color: '#fff'}} />
+                        <p>Konnalath House, Cheruvadi PO, <br /> Kozhikode, Kerala, 673661</p>
+                    </div>
+                    <div className='email contact-details-child'>
+                        <EmailIcon fontSize='large' sx={{color: '#fff'}} />
+                        <p>nskonnalath@gmail.com <br /> nahyan@emeacollege.ac.in</p>
+                    </div>
+                    <div className='phone contact-details-child'>
+                        <CallIcon fontSize='large' sx={{color: '#fff'}} />
+                        <p>+91 9895 790 126</p>
+                    </div>
+                </div>
+
+            </div>
             <SocialMedia />
             <div className='footer-copyright'>
                 <p>© 2022 NAHYAN. All rights reserved.</p>
