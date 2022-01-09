06586a0e4d0c3838f539cc1e854d646777ca3dfb
Nahyan Sharvin <nskonnalath@gmail.com>
2022-01-09 10:20:17 +0530

    feat: navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css'
 
 //Icons
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 
 import logo from '../../assets/logo.svg'
 
+const Menu = () => (
+    <>
+    <p><a href='#home'>Home</a></p>
+    <p><a href='#services'>Services</a></p>
+    <p><a href='#works'>Works</a></p>
+    <p><a href='#about'>About</a></p>
+    </>
+)
+
 function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+
     return (
         <div className='portfolio-navbar'>
             <div className='portfolio-navbar-links'>
-                <div className='portfolio-navbar-links-logo'>
+                {/* <div className='portfolio-navbar-links-logo'>
                     <img src={logo} alt="Logo" />
-                </div>
+                </div> */}
                 <div className='portfolio-navbar-links-container'>
-                    <p><a href='#home'>Home</a></p>
-                    <p><a href='#services'>Services</a></p>
-                    <p><a href='#works'>Works</a></p>
-                    <p><a href='#about'>About</a></p>
+                    <Menu />
                 </div>
                 <div className='portfolio-navbar-ctabtn'>
                     <button type='button'>Get in touch</button>
+                </div>
+                <div className='portfolio-navbar-menu'>
+                    {toggleMenu ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+                    : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+                    }
+                    {toggleMenu && (
+                        <div className='portfolio-navbar-menu-container scale-up-tr'>
+                            <div className='portfolio-navbar-menu-container-links'>
+                                <Menu />
+                                <div className='portfolio-navbar-menu-container-links-ctabtn'>
+                                    <button type='button'>Get in touch</button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
