64bfef3a431bf456399801387b19572b25e82d7a
Nahyan Sharvin <nskonnalath@gmail.com>
2022-01-11 23:48:04 +0530

    feat: service card updated

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -7,7 +7,7 @@ function Service({ icon, title, text }) {
         <div className='service-grid-icon'>
             <img src={icon} alt="Icon" />
         </div> 
-        <h2 className='gradient-text'>{title}</h2>
+        <h2>{title}</h2>
         <p>{text}</p>
         </div>
     )
